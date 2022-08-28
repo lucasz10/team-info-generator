7cf205167b4092478bfaf0c30569cc4ddb8db2f9
lucasz10 <lucas.zach10@gmail.com>
2022-08-28 13:17:33 -0700

    Added comments for future changes. added dist to gitignore for now.

---
+++ index.js
@@ -0,0 +1,5 @@
+//TODO: Create inquirer asking for user input on team manager
+
+//TODO: Create inquirer to ask if user wants to add additional employees. Terminate program if user selects no
+
+//TODO: Create inquirer to asking for input on new employee
---
+++ src/generateHTML.js
@@ -0,0 +1,7 @@
+//TODO: Create function to write file after manager information has been entered
+//This should include the head element as well and the creation of the top of the HTML file
+
+//TODO: Create function to append file after employee information has been entered
+//This should create cards containing the employee information
+
+//TODO: Create function to append file with closing elements after the user terminates the script
